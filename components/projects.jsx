df16f5bac42f7376b5c8ac498611347af910d3dd
devcarlan <devcarlan@gmail.com>
2022-06-06 19:59:24 -0400

    working on projects section

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,9 +1,19 @@
 export const Projects = () => {
+  const projects = [
+    { name: 'AMSVans' },
+    { name: 'Champion Pressure Washing' },
+    { name: 'Casual Streams' },
+  ]
   return (
     <div className='container mx-auto px-32 pb-32'>
       <h3 className='text-2xl border-solid border-l-8 border-l-emerald-600 px-2'>
         Projects
       </h3>
+      <div>
+        {projects.map((project) => (
+          <p>{project.name}</p>
+        ))}
+      </div>
     </div>
   )
 }

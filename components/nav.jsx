9cde4184e274b71a9a7afa0a2c461d4078fe0b62
devcarlan <devcarlan@gmail.com>
2022-06-06 16:59:18 -0400

    setting up nav basics complete

--- components/nav.jsx
+++ components/nav.jsx
@@ -5,15 +5,21 @@ export const Nav = () => {
     { name: 'About', to: '/about' },
     { name: 'Skills', to: '/skills' },
     { name: 'Projects', to: '/projects' },
+    { name: 'Contact', to: '/contact' },
   ]
 
   return (
-    <div className='lg:inline-flex'>
-      {links.map((link, i) => (
-        <Link key={i} href={link.to} passHref>
-          <a className='text-zinc-800 hover:text-emerald-600'>{link.name}</a>
-        </Link>
-      ))}
+    <div className='lg:inline-flex min-w-full bg-slate-100 justify-between items-center'>
+      <div className='bg-emerald-600 ml-4 w-8 h-8'></div>
+      <nav className='inline-flex justify-between'>
+        {links.map((link, i) => (
+          <Link key={i} href={link.to} passHref>
+            <a className='p-4 text-zinc-800 hover:text-emerald-600'>
+              {link.name}
+            </a>
+          </Link>
+        ))}
+      </nav>
     </div>
   )
 }

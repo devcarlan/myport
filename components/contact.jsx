62194d0d0b4a5e267771642932d13ce5b1b07aa4
devcarlan <devcarlan@gmail.com>
2022-06-07 15:07:38 -0400

    beginning work on contact form

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,9 +1,12 @@
+import { ContactForm } from './shared/forms/contactform'
+
 export const Contact = () => {
   return (
     <div className='container mx-auto md:max-w-5xl md:mt-24'>
       <h3 className='text-2xl border-solid border-l-8 border-l-emerald-600 px-2'>
         Contact Me
       </h3>
+      <ContactForm />
     </div>
   )
 }

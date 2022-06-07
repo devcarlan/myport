f3c043a398e29139c561cee5b9b52f40e8612382
devcarlan <devcarlan@gmail.com>
2022-06-07 15:05:04 -0400

    beginning work on contact section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Intro } from '../components/intro'
 import { Skills } from '../components/skills'
 import { Projects } from '../components/projects'
 import { About } from '../components/about'
+import { Contact } from '../components/contact'
 import { Footer } from '../components/footer'
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <About />
+      <Contact />
       <Footer />
     </div>
   )

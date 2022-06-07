98937ed5ab4851181c536d84983cc39b4804d59b
devcarlan <devcarlan@gmail.com>
2022-06-07 15:20:56 -0400

    working on creating the contact form, installed nodemailer dependency for sending emails

--- components/shared/forms/contactform.jsx
+++ components/shared/forms/contactform.jsx
@@ -1,3 +1,67 @@
+import { useState } from 'react'
+
 export const ContactForm = () => {
+  const initialValues = {
+    name: '',
+    email: '',
+    company: '',
+    message: '',
+  }
+
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState({})
+  const [form, setForm] = useState('')
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFormErrors(validate(formValues))
+
+    if (
+      formValues.name &&
+      formValues.email &&
+      formValues.address &&
+      formValues.city &&
+      formValues.state &&
+      formValues.zip &&
+      formValues.message
+    ) {
+      setForm({ state: 'loading' })
+      try {
+        const res = await fetch(`api/contactme`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formValues),
+        })
+
+        const { error } = await res.json()
+
+        if (error) {
+          setForm({
+            state: 'error',
+            message: error,
+          })
+          return
+        }
+
+        setForm({
+          state: 'success',
+          message: 'Your message was sent successfully.',
+        })
+        setFormValues(initialValues)
+      } catch (error) {
+        setForm({
+          state: 'error',
+          message: 'Something went wrong.',
+        })
+      }
+    }
+  }
   return <div>Contact</div>
 }
---
+++ pages/api/contactme.js
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer'
+
+export default async (req, res) => {
+  const { name, email, company, message } = req.body
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  })
+
+  try {
+    await transporter.sendMail({
+      from: email,
+      to: 'devcarlan@gmail.com',
+      subject: `New Contact Form Submission from ${name}`,
+      html: `<p>You have a contact form submission</p><br>
+      <p><strong>${name} </strong>${
+        company ? `from <strong>${company}</strong>` : ''
+      } has sent you a message.</p><br>
+      <p><strong>Email: </strong> ${email}</p><br>
+      <p><strong>Message: </strong> ${message}</p><br>`,
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() })
+  }
+  return res.status(200).json({ error: '' })
+}

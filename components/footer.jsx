51c44e3ccee2b92a5c1735402a5e7bbf46348f53
devcarlan <devcarlan@gmail.com>
2022-06-08 01:01:48 -0400

    adding footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-  return <div className='container mx-auto px-32'>Le footeuh</div>
+  return (
+    <div className='container flex justify-center items-center mx-auto md:max-w-5xl md:mt-8 md:mb-8'>
+      <p className='text-sm text-gray-400'>
+        &copy; {new Date().getFullYear()} Carlan Henry. All rights reserved.
+      </p>
+    </div>
+  )
 }

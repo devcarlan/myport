214ca7c6506222cdaf51b7694591a6e3e3ea2f8a
devcarlan <devcarlan@gmail.com>
2022-06-12 15:11:03 -0400

    adding mobile styles to footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className='container flex justify-center items-center mx-auto md:max-w-5xl md:mt-8 md:mb-8'>
+    <div className='mt-8 pb-8 container flex justify-center items-center mx-auto md:max-w-5xl md:mt-8 md:mb-8 md:pb-0'>
       <p className='text-sm text-gray-400'>
         &copy; {new Date().getFullYear()} Carlan Henry. All rights reserved.
       </p>

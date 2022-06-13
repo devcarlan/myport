3ade1d122f00adf65a7ed48abb57907901a6dc6f
devcarlan <devcarlan@gmail.com>
2022-06-13 16:09:17 -0400

    adding burger menu below medium breakpoint

--- components/nav.jsx
+++ components/nav.jsx
@@ -9,9 +9,26 @@ export const Nav = () => {
   ]
 
   return (
-    <div className='lg:inline-flex min-w-full justify-between items-center md:mt-2 md:mb-2'>
+    <div className='inline-flex min-w-full justify-between items-center py-4'>
       <div className='bg-emerald-600 ml-4 w-8 h-8'></div>
-      <nav className='inline-flex justify-between'>
+      <div className='px-4 md:hidden'>
+        <svg
+          className='w-8'
+          xmlns='http://www.w3.org/2000/svg'
+          class='h-6 w-6'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke='currentColor'
+          stroke-width='2'
+        >
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M4 6h16M4 12h16M4 18h16'
+          />
+        </svg>
+      </div>
+      <nav className='hidden md:inline-flex justify-between'>
         {links.map((link, i) => (
           <Link key={i} href={link.to} passHref>
             <a className='p-4 text-zinc-800 hover:text-emerald-600'>

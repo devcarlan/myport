6037182f9b66056d1393d6f98d78a8009fd1ea25
devcarlan <devcarlan@gmail.com>
2022-06-29 17:34:25 -0400

    adding layout to projects page

---
+++ components/layouts/projectlayout.jsx
@@ -0,0 +1,18 @@
+import { Nav } from '../nav'
+import { Footer } from '../footer'
+
+const ProjectLayout = ({ children }) => {
+  return (
+    <>
+      <div className='bg-gray-200 max-w-screen-xl mx-auto'>
+        <Nav />
+      </div>
+      <div className='wrapper'>{children}</div>
+      <div className='hidden max-w-screen-xl mx-auto md:block'>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default ProjectLayout
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,8 +1,9 @@
+import ProjectLayout from '../../components/layouts/projectlayout'
 const Projects = () => {
   return (
-    <>
+    <ProjectLayout>
       <h1>Projects</h1>
-    </>
+    </ProjectLayout>
   )
 }
 export default Projects

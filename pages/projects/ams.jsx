94ac8a3a8b51c0fb8536965d08cb5909a7ae1fa5
devcarlan <devcarlan@gmail.com>
2022-07-06 17:25:31 -0400

    finishing up AMS project page

--- pages/projects/ams.jsx
+++ pages/projects/ams.jsx
@@ -1,17 +1,51 @@
 const AMS = () => {
   return (
     <>
-      <div className='h-[500px] container flex flex-col items-center justify-center bg-ams text-white'>
-        <h1 className='text-7xl after:block after:content-[""] after:border-4 after:border-emerald-600 after:w-2/4 after:m-auto'>
+      <div className='h-[500px] flex flex-col items-center justify-center bg-ams text-white'>
+        <h1 className='text-7xl after:block after:content-[""] after:border-4 after:border-emerald-600 after:w-2/5 after:m-auto'>
           AMS Vans
         </h1>
       </div>
-      <div className='container pt-4 px-4 m-auto md:max-w-5xl'>
-        <p>Visit Site: Link</p>
-        <p>Technologies Used: Wordpress</p>
-        <p>Languages Used: HTML, CSS, PHP, JavaScript</p>
-        <p>My Role:</p>
-        <p>Visit Site</p>
+      <div className='container p-4 m-auto md:max-w-5xl'>
+        <p className='m-6 text-lg md:mx-2 md:my-4 hover:pointer'>
+          <span className='font-semibold'>Visit Site:</span>{' '}
+          <a
+            className='text-emerald-600 hover:text-emerald-400'
+            href='https://www.amsvans.com'
+            target='_blank'
+          >
+            AMS Vans
+          </a>
+        </p>
+        <p className='m-6 text-lg md:mx-2 md:my-4'>
+          <span className='font-semibold'>Technologies:</span> WordPress
+        </p>
+        <p className='m-6 text-lg md:mx-2 md:my-4'>
+          <span className='font-semibold'>Languages:</span> HTML, CSS, PHP,
+          JavaScript
+        </p>
+        <p className='m-6 text-lg md:mx-2 md:mt-4 md:mb-2'>
+          <span className='font-semibold'>My Role: </span>
+          On this project, as the junior developer, my responsibilities
+          included:
+        </p>
+        <ul className='mx-6 text-lg list-outside md:list-inside list-disc'>
+          <li className='my-4'>
+            Creating page templates for the administrative backend for editing
+            and adding new content.
+          </li>
+          <li className='my-4'>
+            Porting page content from legacy system to the WordPress
+            environment. Verbiage, images, and links etc.
+          </li>
+          <li className='my-4'>
+            Coordinating with the marketing team to deploy content changes from
+            the staging site to the production build.
+          </li>
+          <li className='my-4'>
+            Creating a vehicle finance calculator using JQuery.
+          </li>
+        </ul>
       </div>
     </>
   )

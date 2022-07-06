8529c9d3b768ffad546c8bca48bd491ed32e227e
devcarlan <devcarlan@gmail.com>
2022-07-06 14:08:10 -0400

    working on ams vans project page

--- pages/projects/ams.jsx
+++ pages/projects/ams.jsx
@@ -1,9 +1,13 @@
 const AMS = () => {
   return (
-    <div className='h-[500px] container flex flex-col items-center justify-center bg-ams text-white'>
-      <h1>AMS Vans</h1>
-      <h2>Under Contstruction</h2>
-    </div>
+    <>
+      <div className='h-[500px] container flex flex-col items-center justify-center bg-ams text-white'>
+        <h1 className='text-7xl after:block after:content-[""] after:border-4 after:border-emerald-600 after:w-2/4 after:m-auto'>
+          AMS Vans
+        </h1>
+      </div>
+      <div></div>
+    </>
   )
 }
 

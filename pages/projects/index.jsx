27a21f28b299dd7f2cdae10d854d18af56a9c730
devcarlan <devcarlan@gmail.com>
2022-10-06 21:29:44 -0400

    html safe

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -38,15 +38,15 @@ function Projects() {
   return (
     <>
       <Head>
-        <title>Carlan Henry's Projects</title>
+        <title>Carlan Henry&apos;s Projects</title>
       </Head>
       <div className='px-2 flex flex-col container justify-center items-center md:px-4'>
         <h1 className='m-4 text-emerald-600 md:m-8'>Projects</h1>
         <p className='w-4/5 tracking-wide text-center md:w-2/3'>
-          These are <em>almost</em> all of my projects. Recently I've been
+          These are <em>almost</em> all of my projects. Recently I&apos;ve been
           focusing on React and Next.js based projects. Click a project card to
-          visit the project's page where you can view additional details as well
-          as check out the live demo.
+          visit the project&apos;s page where you can view additional details as
+          well as check out the live demo.
         </p>
         <div className='mt-4 flex flex-col items-center md:flex-row md:flex-wrap md:justify-center md:mt-8'>
           {projects.map((project, i) => (
